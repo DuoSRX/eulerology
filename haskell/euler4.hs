b26c4616fd18a73b8077a8431d218d5c2f1b4548
Xavier Perez <duosrx@gmail.com>
2011-03-30 00:07:18 +0200

    [Haskell] Solved problem 4

---
+++ haskell/euler4.hs
@@ -0,0 +1,9 @@
+-- euler 4
+-- http://projecteuler.net/index.php?section=problems&id=4
+-- answer: ?
+
+euler4 :: Integer
+euler4 = maximum $ filter pal [x*y | x <- [999,998..100], y <- [999,998..100]]
+
+pal :: Integer -> Bool
+pal n = show n == reverse(show n)
