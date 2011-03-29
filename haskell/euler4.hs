cb4d5e7b2a9b83360c1bdeb8d29ce3de916b4071
Xavier Perez <duosrx@gmail.com>
2011-03-30 00:33:40 +0200

    [Haskell] added the missing answer in problem 4

--- haskell/euler4.hs
+++ haskell/euler4.hs
@@ -1,6 +1,6 @@
 -- euler 4
 -- http://projecteuler.net/index.php?section=problems&id=4
--- answer: ?
+-- answer: 906609
 
 euler4 :: Integer
 euler4 = maximum $ filter pal [x*y | x <- [999,998..100], y <- [999,998..100]]
