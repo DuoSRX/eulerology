40a0dacf05c10f39054fa75002067746ec9584c7
Xavier Perez <duosrx@gmail.com>
2011-05-05 13:23:58 +0200

    [Haskell] Simplified answer to problem 48

--- haskell/euler48.hs
+++ haskell/euler48.hs
@@ -2,13 +2,6 @@
 -- http://projecteuler.net/index.php?section=problems&id=48
 -- answer: 9110846700
 
+-- way simpler than the privous answer
+euler48 = reverse . take 10 . reverse . show $ sum [x^x | x <- [1..1000]]
 
--- taken from http://hackage.haskell.org/packages/archive/digits/0.1/doc/html/src/Data-Digits.html#digits
-digitsRev i = case i of
-        0 -> []
-        _ -> lastDigit : digitsRev rest
-    where (rest, lastDigit) = quotRem i 10
-
-unDigits = foldl (\ a b -> a * 10 + b) 0
-
-euler48 = unDigits . reverse . take 10 $ digitsRev $ sum [x^x | x <- [1..1000]]
